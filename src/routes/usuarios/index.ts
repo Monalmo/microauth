ac4a5643fdefed849e91e502ba554e9f032170a6
Monalmo <cristian.hadad.g@gmail.com>
2022-05-08 11:47:15 -0400

    update: actualiza librerias deprecadas

--- src/routes/usuarios/index.ts
+++ src/routes/usuarios/index.ts
@@ -1,8 +1,8 @@
 import { FastifyPluginAsync } from 'fastify'
 
 const example: FastifyPluginAsync = async (fastify): Promise<void> => {
-	fastify.get('/', async function (request, reply) {
-		// console.log(request)
+	fastify.post('/', async function (request, reply) {
+		// console.log('body: ',request.body)
 
 		return reply.send({ hjh: 'aca se entrega un usuario' })
 	})
